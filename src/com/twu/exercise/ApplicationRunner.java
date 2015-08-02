7d0a69344aaeb328cd25b73c7953dda532b53cab
Waterstrong <sqlin@thoughtworks.com>
2015-08-02 13:33:10 +0800

    [Waterstrong] - finish triangle exercise.

--- src/com/twu/exercise/ApplicationRunner.java
+++ src/com/twu/exercise/ApplicationRunner.java
@@ -5,6 +5,18 @@ import com.twu.exercise.triangle.TriangleExercise;
 public class ApplicationRunner {
     public static void main(String[] args) {
         TriangleExercise triangle = new TriangleExercise();
-        triangle.printAsterisk();
+        triangle.drawAsterisk();
+        printSplitLine();
+        triangle.drawHorizontalLine(8);
+        printSplitLine();
+        triangle.drawVerticalLine(3);
+        printSplitLine();
+        triangle.drawRightTriangle(3);
+        printSplitLine();
+
+    }
+
+    private static void printSplitLine() {
+        System.out.println("---------华丽丽的分割线-----------");
     }
 }
--- src/com/twu/exercise/triangle/TriangleExercise.java
+++ src/com/twu/exercise/triangle/TriangleExercise.java
@@ -2,7 +2,32 @@ package com.twu.exercise.triangle;
 
 public class TriangleExercise {
 
-    public void printAsterisk() {
-        System.out.println("*");
+    private void printAsterisk() {
+        System.out.print("*");
+    }
+
+    public void drawAsterisk() {
+        printAsterisk();
+        System.out.println();
+    }
+
+    public void drawHorizontalLine(int n) {
+        for (int i = 0; i < n; ++i) {
+            printAsterisk();
+        }
+        System.out.println();
+    }
+
+    public void drawVerticalLine(int n) {
+        for(int i = 0; i < n; ++i) {
+            printAsterisk();
+            System.out.println();
+        }
+    }
+
+    public void drawRightTriangle(int n) {
+        for (int i = 0; i < n; ++i) {
+            drawHorizontalLine(i+1);
+        }
     }
 }
