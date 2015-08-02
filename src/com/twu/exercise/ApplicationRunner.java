bb8cd7970e2c87d6a6f5a659d09219a1dcaf4f5c
Waterstrong <sqlin@thoughtworks.com>
2015-08-02 13:17:21 +0800

    [Waterstrong] - add application runner, print asterisk, and update gradle.

---
+++ src/com/twu/exercise/ApplicationRunner.java
@@ -0,0 +1,10 @@
+package com.twu.exercise;
+
+import com.twu.exercise.triangle.TriangleExercise;
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        TriangleExercise triangle = new TriangleExercise();
+        triangle.printAsterisk();
+    }
+}
---
+++ src/com/twu/exercise/triangle/TriangleExercise.java
@@ -0,0 +1,8 @@
+package com.twu.exercise.triangle;
+
+public class TriangleExercise {
+
+    public void printAsterisk() {
+        System.out.println("*");
+    }
+}
